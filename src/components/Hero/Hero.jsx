5a31121c6f0b91f838a4fbbea1313bca4e1414a9
Harikalyan99 <harikalyan99tronics@gmail.com>
2024-01-24 13:10:08 +0530

    some tweaks in the hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,12 +6,12 @@ import HeroImage from '../../assets/vibrating-headphone 1.png';
 const Hero = () => {
   return (
     <div className={styles.hero}>
-        <div> 
+        <div style={{height: "100px"}}> 
             <h1>100 Thousand Songs, ad-free</h1>
             <h1>Over thousands podcast episodes</h1>
         </div>
         <div>
-            <img src={HeroImage} alt="imageHeadphones" width={212}/>
+            <img src={HeroImage} alt="imageHeadphones" width={212} height={212}/>
         </div>
     </div>
   )
